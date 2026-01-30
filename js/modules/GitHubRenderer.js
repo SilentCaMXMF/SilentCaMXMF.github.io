ac2a46213d451fd476cf6c2d898522cc04edee23
SilentCaMXMF <pedroocalado@gmail.com>
2026-01-30 13:03:54 +0000

    fix: Remove duplicate EventManager class declaration
    - Create separate EventManager.js module
    - Remove duplicate class from GitHubRenderer.js
    - Fix SyntaxError on deployment

--- js/modules/GitHubRenderer.js
+++ js/modules/GitHubRenderer.js
@@ -455,29 +455,3 @@ export class GitHubRenderer {
         this.reposWithoutDesc = [];
     }
 }
-
-/**
- * Simple Event Manager for custom events
- */
-class EventManager {
-    constructor() {
-        this.listeners = new Map();
-    }
-
-    on(event, callback) {
-        if (!this.listeners.has(event)) {
-            this.listeners.set(event, []);
-        }
-        this.listeners.get(event).push(callback);
-    }
-
-    emit(event, data) {
-        if (this.listeners.has(event)) {
-            this.listeners.get(event).forEach(callback => callback(data));
-        }
-    }
-
-    removeAllListeners() {
-        this.listeners.clear();
-    }
-}
